1d54f49a05cf8ea0ee94cd751c25dd7b0abe23f6
mkwon1203 <mkwon1203@email.arizona.edu>
2015-04-23 21:28:02 -0700

    updated addHealth method on all characters

--- project/src/model/Priest.java
+++ project/src/model/Priest.java
@@ -35,22 +35,31 @@ public class Priest extends Character {
 		 * 
 		 * case 2 and 3 returns true
 		 */
-		if (getHealth() == PRIEST_HEALTH || !isAlive())
+		if (!isAlive())
 			// case 1
 			return false;
-		else if (getHealth() + delta > PRIEST_HEALTH)
-			// case 2
-			setHealth(PRIEST_HEALTH);
-		else if (getHealth() + delta <= 0)
+		if (delta > 0)
 		{
-			// case 3
-			setHealth(0);
-			setDead();
+			// can't add to full health unit
+			if (getHealth() == PRIEST_HEALTH)
+				return false;
+			else if (getHealth() + delta > PRIEST_HEALTH)
+				// case 2
+				setHealth(PRIEST_HEALTH);
 		}
+		// else delta is <= 0
 		else
-			setHealth(getHealth() + delta);
+		{
+			if (getHealth() + delta <= 0)
+			{
+				// case 3
+				setHealth(0);
+				setDead();
+			}
+			else
+				setHealth(getHealth() + delta);
+		}
 		return true;
-
 	}
 
 	@Override
--- project/src/model/Spearman.java
+++ project/src/model/Spearman.java
@@ -36,22 +36,31 @@ public class Spearman extends Character {
 		 * 
 		 * case 2 and 3 returns true
 		 */
-		if (getHealth() == SPEARMAN_HEALTH || !isAlive())
+		if (!isAlive())
 			// case 1
 			return false;
-		else if (getHealth() + delta > SPEARMAN_HEALTH)
-			// case 2
-			setHealth(SPEARMAN_HEALTH);
-		else if (getHealth() + delta <= 0)
+		if (delta > 0)
 		{
-			// case 3
-			setHealth(0);
-			setDead();
+			// can't add to full health unit
+			if (getHealth() == SPEARMAN_HEALTH)
+				return false;
+			else if (getHealth() + delta > SPEARMAN_HEALTH)
+				// case 2
+				setHealth(SPEARMAN_HEALTH);
 		}
+		// else delta is <= 0
 		else
-			setHealth(getHealth() + delta);
+		{
+			if (getHealth() + delta <= 0)
+			{
+				// case 3
+				setHealth(0);
+				setDead();
+			}
+			else
+				setHealth(getHealth() + delta);
+		}
 		return true;
-
 	}
 
 	@Override
