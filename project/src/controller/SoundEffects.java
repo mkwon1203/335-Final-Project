fc02952fe491b50b0972a24fc66c68445d62496f
CGrundy <grundy@email.arizona.edu>
2015-05-06 14:13:01 -0700

    Added sound to title menus and main game.

--- project/src/controller/SoundEffects.java
+++ project/src/controller/SoundEffects.java
@@ -14,11 +14,17 @@ import java.util.*;
 public class SoundEffects {
    
    private static ArrayList<Clip> clips = new ArrayList<Clip>();
+   public static ArrayList<String> clipNames = new ArrayList<String>();
+   
+   private SoundEffects(){
+	   
+   }
    
    	public static void addSound(String soundFileName) {
 		try {
 			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundFileName));
 			clips.add(AudioSystem.getClip());
+			clipNames.add(soundFileName);
          
 			Clip currClip = clips.get(clips.size() - 1);
          
@@ -47,11 +53,46 @@ public class SoundEffects {
 	   		for(int i = 0; i < cleanup.size(); i++) {
 	   			if (clips.size() > (int) cleanup.get(i)) {
 	   				Clip currClip = clips.get((int) cleanup.get(i));
+	   				clipNames.remove((int)cleanup.get(i));
+
+	   				clips.get((int)cleanup.get(i)).close();
 	   				clips.remove((int) cleanup.get(i));
 	   			}
 	   		}
    		}
 
+   	}
+   	
+   	public static void clearSoundClips(){
+   		for(int i = 0; i < clips.size(); i++){
+   			clips.get(i).stop();
+   			clips.remove(i);
+   		}
+   	}
+   	
+   	public static boolean containsSoundClip(String soundFileName){
+   		
+   		for(String s : clipNames){
+   			if(s.equals(soundFileName))
+   				return true;
+   		}
+   		return false;
+   	}
+   	
+   	public static void removeSoundClip(String soundFileName){
+   		
+   		for(int i = 0; i < clipNames.size(); i++){
+   			if(clipNames.get(i).equals(soundFileName)){
+   				clips.get(i).stop();
+   				clips.remove(i);
+   				clipNames.remove(i);
+   			}
+   		}
+   		
+   	}
+   	
+   	public static int getSoundClipArraySize(){
+   		return clips.size();
    	}
    	
    	public static void showSize() {
