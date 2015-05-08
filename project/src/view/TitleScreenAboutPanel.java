959dc06b2831750e1cfa1c8f679c975b7de46bfb
josuemorga <josuemorga@email.arizona.edu>
2015-05-07 17:56:08 -0700

    added a tutorial

--- project/src/view/TitleScreenAboutPanel.java
+++ project/src/view/TitleScreenAboutPanel.java
@@ -20,11 +20,12 @@ import javax.swing.JTextArea;
 public class TitleScreenAboutPanel extends JPanel
 {
 	private Image background;
-	private Image backButtonImage;
-	private JButton backButton;
+	private Image backButtonImage, nextButtonImage, quitButtonImage;
+	private JButton backButton, tutorialButton, nextButton, backTutorial, quitTutorial;
 	private JTextArea aboutLabel;
 	private MenuListener listener;
 	private JScrollPane aboutScrollPane;
+	private int aboutState = 0;
 	
 	private TitleScreen title;
 	
@@ -53,15 +54,44 @@ public class TitleScreenAboutPanel extends JPanel
     
 	private void loadBackground()
 	{
-		try
-		{
-			background = ImageIO.read(new File(
-                                               "res/titleScreen/titleBackground.png"));
-			// backgroundLabel = new JLabel(new ImageIcon(titlePic));
+		if (aboutState == 0){
+			try
+			{
+				background = ImageIO.read(new File(
+                                                   "res/titleScreen/titleBackground.png"));
+				// backgroundLabel = new JLabel(new ImageIcon(titlePic));
+			}
+			catch (IOException e)
+			{
+				System.out.println("Couldn't find title picture.");
+			}
 		}
-		catch (IOException e)
-		{
-			System.out.println("Couldn't find title picture.");
+		
+		// attack and move tutorial
+		else if (aboutState == 1){
+			try{
+				background = ImageIO.read(new File("res/titleScreen/Attack&MoveTutorial.png"));
+			}
+			catch (IOException e1){
+				System.out.println("Couldn't find button image.");
+			}
+		}
+		// item tutorial
+		else if (aboutState == 2){
+			try{
+				background = ImageIO.read(new File("res/titleScreen/ItemTutorial.png"));
+			}
+			catch (IOException e1){
+				System.out.println("Couldn't find button image.");
+			}
+		}
+		else if (aboutState == 3){
+			try{
+				background = ImageIO.read(new File("res/titleScreen/SkillTutorial.png"));
+			}
+			catch (IOException e1){
+				System.out.println("Couldn't find button image.");
+			}
 		}
 	}
     
@@ -72,6 +102,10 @@ public class TitleScreenAboutPanel extends JPanel
 		{
 			backButtonImage = ImageIO.read(new File(
                                                     "res/titleScreen/back.png"));
+			nextButtonImage = ImageIO.read(new File(
+                    								"res/titleScreen/next.png"));
+			
+			quitButtonImage = ImageIO.read(new File("res/titleScreen/quit.png"));
             
 			backButton = new JButton(new ImageIcon(backButtonImage));
             
@@ -84,19 +118,70 @@ public class TitleScreenAboutPanel extends JPanel
 			backButton.setName("back");
 			
 			backButton.addActionListener(listener);
+			
+			// back button in tutorial
+			backTutorial = new JButton(new ImageIcon(backButtonImage));
+			backTutorial.setOpaque(false);
+			backTutorial.setContentAreaFilled(false);
+			backTutorial.setBorderPainted(false);
+			backTutorial.setRolloverIcon(new ImageIcon(ImageIO.read(new File("res/titleScreen/back2.png"))));
+			
+			// next button in tutorial
+			nextButton = new JButton(new ImageIcon(nextButtonImage));
+			nextButton.setOpaque(false);
+			nextButton.setContentAreaFilled(false);
+			nextButton.setBorderPainted(false);
+			nextButton.setRolloverIcon(new ImageIcon(ImageIO.read(new File(
+                                                                           "res/titleScreen/next2.png"))));
+			
+			// quit tutorial button
+			quitTutorial = new JButton(new ImageIcon(quitButtonImage));
+			quitTutorial.setOpaque(false);
+			quitTutorial.setContentAreaFilled(false);
+			quitTutorial.setBorderPainted(false);
+			quitTutorial.setRolloverIcon(new ImageIcon(ImageIO.read(new File("res/titleScreen/quit2.png"))));
 		}
 		catch (IOException e)
 		{
 			System.out.println("Couldn't find button image.");
 		}
+		
+		// adding the tutorial button
+		tutorialButton = new JButton("Tutorial");
+		tutorialButton.setName("tutorial");
+		tutorialButton.addActionListener(listener);
+		
+		nextButton.setName("next");
+		nextButton.addActionListener(listener);
+		
+		backTutorial.setName("backTutorial");
+		backTutorial.addActionListener(listener);
+		
+		quitTutorial.setName("quitTutorial");
+		quitTutorial.addActionListener(listener);
 	}
     
 	private void addButton()
 	{
 		this.add(backButton);
         
-		backButton.setBounds(500, 100, backButtonImage.getWidth(null),
+		backButton.setBounds(500, 550, backButtonImage.getWidth(null),
                              backButtonImage.getHeight(null));
+		
+		this.add(tutorialButton);
+		tutorialButton.setBounds(500, 100, 100, 50);
+		
+		this.add(nextButton);
+		nextButton.setBounds(625, 550, 100, 50);
+		nextButton.setVisible(false); // want this button to appear after you click tutorial
+		
+		this.add(backTutorial);
+		backTutorial.setBounds(500, 550, 100, 50);
+		backTutorial.setVisible(false);
+		
+		this.add(quitTutorial);
+		quitTutorial.setBounds(625,550,100,50);
+		quitTutorial.setVisible(false);
 	}
 	
 	private void loadTextArea()
@@ -135,7 +220,60 @@ public class TitleScreenAboutPanel extends JPanel
 				{
 					TitleScreen.TITLESTATE = 0;
 					title.draw();
+				} // adding code for tutorial
+				else if (button.getName() == "tutorial"){
+                    aboutState = 1;
+                    loadBackground();
+                    repaint();
+                    tutorialButton.setVisible(false);
+                    aboutLabel.setVisible(false);
+                    backButton.setVisible(false);
+                    nextButton.setVisible(true);
+                    backTutorial.setVisible(true);
+                }
+				else if (button.getName() == "next"){
+					if (aboutState < 3)
+						aboutState++;
+					loadBackground();
+					repaint();
+					tutorialButton.setVisible(false);
+					aboutLabel.setVisible(false);
+					backButton.setVisible(false);
+					if (aboutState == 3){
+						nextButton.setVisible(false);
+						quitTutorial.setVisible(true);
+					}
+					backTutorial.setVisible(true);
 				}
+				else if (button.getName() == "backTutorial"){
+					if (aboutState > 0)
+						aboutState--;
+					loadBackground();
+					repaint();
+                    backButton.setVisible(false);
+                    nextButton.setVisible(true);
+                    if (aboutState == 0){
+                        tutorialButton.setVisible(true);
+                        aboutLabel.setVisible(true);
+                        backTutorial.setVisible(false);
+                        nextButton.setVisible(false);
+                        backButton.setVisible(true);
+                    }
+                    quitTutorial.setVisible(false);
+                    
+				}
+				else if (button.getName() == "quitTutorial"){
+					aboutState = 0;
+					loadBackground();
+					aboutLabel.setVisible(true);
+					backButton.setVisible(true);
+					backTutorial.setVisible(false);
+					nextButton.setVisible(false);
+					quitTutorial.setVisible(false);
+					tutorialButton.setVisible(true);
+					repaint();
+				}
+                
 			}
 		} // end of ActionPerformed
 	} // end of MenuListener
